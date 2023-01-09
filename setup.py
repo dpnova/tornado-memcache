f665c425a6596d9b6b1470ca29e799d41e10ac27
Ganesh Prasad <gprasad@wish.com>
2023-01-09 14:42:18 -0800

    debug3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(name='TornadoAsyncMemcache',
       py_modules=['tornadoasyncmemcache'],
-      version=100.1,
+      version=100.2,
       description="Async driver for memcache and tornado.",
       author="David P. Novakovic",
       author_email="dpn@dpn.name",
